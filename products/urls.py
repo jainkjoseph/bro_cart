f5be54b8c024a97e0443268277c7dc3b55d9078f
jain <jainibrm@gmail.com>
2025-07-30 01:03:42 +0400

    product list tested ok, actual record addition not finished

--- products/urls.py
+++ products/urls.py
@@ -8,7 +8,6 @@ from . import views
 urlpatterns = [
     
     path ('',views.index),
- 
-    
+    path('product_list',views.list_products,name='list_product'),
    
 ]
--- products/views.py
+++ products/views.py
@@ -4,4 +4,17 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 
+def list_products(request):
+    """
+    products details  display
+    """
+    return render (request,'products.html')
+
+def detail_product(request):
+
+    """
+    product wise details - individual
+    """
+    return render(request,'product_detail.html')
+
  
