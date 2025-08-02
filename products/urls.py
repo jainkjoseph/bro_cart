2e31f607ed8cce107b14d8a34687fc097e3e87af
jain <jainibrm@gmail.com>
2025-08-02 20:18:55 +0400

    user registration for set

--- products/urls.py
+++ products/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 urlpatterns = [
     
-    path ('',views.index),
+    path ('',views.index,name='home'),
     path('product_list',views.list_products,name='list_product'),
-   
+    path('product_detail',views.detail_product,name='detail_product'),
 ]
---
+++ readme.txt
@@ -0,0 +1,8 @@
+to activate environment
+cd ..
+source bro_env/bin/activate
+after activation
+cd bro_cart
+
+to runserver : python3 manage.py runserver
+
