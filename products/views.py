e62cd9e7a39b449264dc9029f2376f7b1591e332
jain <jainibrm@gmail.com>
2025-08-11 21:49:56 +0400

    feature & latest product fixed

--- products/views.py
+++ products/views.py
@@ -4,7 +4,13 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    featured_products = Product.objects.order_by("priority")[:3]
+    latest_products = Product.objects.order_by("-priority")[:3]
+    context={
+        'featured_products' : featured_products,
+        'latest_products'   : latest_products
+    }
+    return render(request,'index.html',context)
 
 def list_products(request):
     """
@@ -15,9 +21,12 @@ def list_products(request):
     if request.GET:
         page = request.GET.get('page',1)
     
-   
+    """
+    DISPLAY ALL
     product_list = Product.objects.all()
-
+    TO DISPLAY PRIORITY BASED
+    """
+    product_list = Product.objects.order_by("priority")
     product_paginator = Paginator(product_list,3)
     product_list=product_paginator.get_page(page)
     response = render(request, 'products.html',{'products': product_list})  
