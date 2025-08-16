c9f3ebed35968aa65333995640de4dcd8637ccf5
jain <jainibrm@gmail.com>
2025-08-16 21:42:55 +0400

    login/Register Screen designed

--- bro_cart/settings.py
+++ bro_cart/settings.py
@@ -119,13 +119,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'brokart/'
 MEDIA_URL = '/images/'
+
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
+"""
+STATICFILES_DIRS =[
+    BASE_DIR/ "static"
+]
+"""
+
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
