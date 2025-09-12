6994a04ae83a4dd0bdcfaad6e75ec7e2f6a89534
jain <jainibrm@gmail.com>
2025-09-12 12:39:37 +0400

    temp

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from orders.models import Customer
 
+admin.site.register(Customer)
 # Register your models here.
--- orders/views.py
+++ orders/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render,redirect
 from .models import Order,OrderedItem
 from django.contrib import messages
-from . models import Product,Customer
+from . models import Product 
+from . models import Customer 
 
 # Create your views here.
 def show_cart(request):
    user=request.user
    customer=user.customer_profile 
+
    cart_obj,created=Order.objects.get_or_create(
-   owner=customer,
-   order_status=Order.CART_STAGE
-   )
+      owner=customer,
+      rder_status=Order.CART_STAGE
+      )
    context = {'cart': cart_obj}
    return render(request,'cart.html',context)
+
+
 def checkout_cart(request):
    if request.POST:
  
@@ -43,8 +47,10 @@ def add_to_cart(request):
    if request.POST:
          user=request.user
          customer=user.customer_profile
+         
          quantity=int(request.POST.get('quantity'))
          product_id=request.POST.get('product_id')
+   
          cart_obj,created=Order.objects.get_or_create(
             owner=customer,
             order_status=Order.CART_STAGE,
@@ -53,7 +59,8 @@ def add_to_cart(request):
          product=Product.objects.get(pk=product_id)
          ordered_item,created=OrderedItem.objects.get_or_create(
             product=product,
-            owner=cart_obj
+            owner=cart_obj,
+            quantity=quantity
          )
          if created:
             ordered_item.quantity=quantity
@@ -62,6 +69,8 @@ def add_to_cart(request):
             ordered_item.quantity=ordered_item.quantity+quantity
             ordered_item.save()
          return redirect('cart')
+
+
 def delete(request,pk):
    
    instance = OrderedItem.objects.get(pk=pk)
